f6f258897cb9b45b9c5c10e49006c118baf4e41a
Leo <gbgleo@gmail.com>
2021-04-21 18:50:18 +0200

    still experimenting

--- input/add_code.py
+++ input/add_code.py
@@ -7,6 +7,15 @@ def listToString(listInput):
     for i in listInput:
         str += i
 
+def checkCode(code):
+
+def combineCodeAndVars(code, variables, positions):
+    saveToFile(combine(code, variables, positions))
+
+def saveToFile(code):
+
+# Initialization of local variables
+
 user_code = input("Enter code: ")
 code_list = ConvertToList(user_code)
 variable_pos = []
@@ -14,18 +23,19 @@ variables = []
 code_with_variables = ""
 counter = 1
 
-
 uniqueVarValues = input(f"How many unique variable values are there in\n\t{user_code}")
-for i in code_list: 
-    part = f"{i}"
-    print("[" + str(counter) + "] : " + part)
-    counter += 1
-variable_pos = ConvertToList(input('Identify the variable values?\n'
-    'Enter the corresponding number, separate with space if there are multiple variable values:\n'))
+
+# Identify the variables for user code
+for value in uniqueVarValues:
+    for i in code_list: 
+        part = f"{i}"
+        print("[" + str(counter) + "] : " + part)
+        counter += 1
+    variable_pos = ConvertToList(input('Identify the values for var1?\n'
+        'Enter the corresponding number, separate with space if there are multiple variable values:\n'))
 
 for i in variable_pos:
     print(code_list[int(i)-1])
-
 
 print(variable_pos)
 for i in variable_pos:
@@ -36,7 +46,9 @@ if uniqueVarValues < 1:
     print(f"Is this the complete code?\n\t{user_code}")
 elif len(variables) > 1 and uniqueVarValues == 1:
     print(f"Is this the complete code\n\t{user_code}")
-elif len(variables) and uniqueVarValues > 1:
+elif len(variables) <= 1 and uniqueVarValues > 1:
     oneVarValue = input("Does these variables share the same value? (y/n)")
     if oneVarValue == "y":
         print("Is this the correct code?\n")
+else:
+    saveToFile(user_code)
